1c91e6f96e01bd05277ea9e44be2ee49f4152631
ofirf <ofirf@sentinel.com>
2019-06-14 20:28:47 +0300

    first commit

---
+++ projects/ngx-ace/src/lib/ngx-ace.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aa-ngx-ace',
+  template: `
+    <p>
+      ngx-ace works!
+    </p>
+  `,
+  styles: []
+})
+export class NgxAceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/ngx-ace/src/lib/ngx-ace.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NgxAceComponent } from './ngx-ace.component';
+
+@NgModule({
+  declarations: [NgxAceComponent],
+  imports: [
+  ],
+  exports: [NgxAceComponent]
+})
+export class NgxAceModule { }
