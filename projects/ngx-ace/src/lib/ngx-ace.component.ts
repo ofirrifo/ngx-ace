28bc290ed18539ee38737d25cc2f5f7130d7e069
ofirf <ofirf@sentinel.com>
2019-06-14 21:20:58 +0300

    create aa-ngx-ace and

--- projects/ngx-ace/src/lib/ngx-ace.component.ts
+++ projects/ngx-ace/src/lib/ngx-ace.component.ts
@@ -1,35 +1,34 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import * as ace from 'ace-builds';
+import 'ace-builds/webpack-resolver';
+
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/theme-github';
-import 'ace-builds/webpack-resolver';
+
 
 const THEME = 'ace/theme/github';
 const LANG = 'ace/mode/javascript';
 
 @Component({
   selector: 'aa-ngx-ace',
-  template: `
-    <div class="code-editor" #codeEditor></div>`,
-  styles: []
+  template: '',
+  styles: [':host { display:block;width:100%; }'],
 })
 export class NgxAceComponent implements OnInit {
-
-  @ViewChild('codeEditor', {static: true}) codeEditorElmRef: ElementRef;
   private codeEditor: ace.Ace.Editor;
 
-  constructor() {
+  constructor(private elementRef: ElementRef) {
+
   }
 
   ngOnInit() {
-    const element = this.codeEditorElmRef.nativeElement;
     const editorOptions: Partial<ace.Ace.EditorOptions> = {
       highlightActiveLine: true,
       minLines: 10,
       maxLines: Infinity,
     };
 
-    this.codeEditor = ace.edit(element, editorOptions);
+    this.codeEditor = ace.edit(this.elementRef.nativeElement, editorOptions);
     this.codeEditor.setTheme(THEME);
     this.codeEditor.getSession().setMode(LANG);
     this.codeEditor.setShowFoldWidgets(true); // for the scope fold feature
